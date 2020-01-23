011d79cfe6ef2d4d195ff242e0f664a1caa6a412
zulfiyebiser <zlfdnbsr@gmail.com>
2020-01-23 16:58:23 -0600

    added new classes

---
+++ src/test/java/com/automation/tests/day2/MetaWeatherTests.java
@@ -0,0 +1,51 @@
+package com.automation.tests.day2;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MetaWeatherTests {
+
+    /**
+     * /api/location/search/?query=san
+     * /api/location/search/?query=london
+     * /api/location/search/?lattlong=36.96,-122.02
+     * /api/location/search/?lattlong=50.068,-5.316
+     * /api/location/44418/ }/
+     * "title": "San Francisco",
+     * "location_type": "City",
+     * "woeid": 2487956,
+     * "latt_long": "37.777119, -122.41964"
+     * },
+     */
+
+    private String baseURI = "https://www.metaweather.com/api/";
+
+    @Test
+    public void test1() {
+        Response response = given()
+                .baseUri(baseURI + "location/search/")
+                .queryParam("query", "New")
+                .get();
+        assertEquals(200, response.getStatusCode());
+        System.out.println(response.prettyPrint());
+
+    }
+
+    // /users/100/ - 100 it's a path parameter
+    // /users/255/ - 155 it's a path parameter
+    // /users/255?name=James | name - query parameter key=value , key it's a query parameter
+    //        "woeid": 2514815,
+    @Test
+    public void test2() {
+        Response response = given()
+                .pathParam("woeid", "2458833")
+                .get(baseURI + "location/{woeid}");
+        System.out.println(response.prettyPrint());
+
+    }
+}
--- src/test/java/com/automation/tests/day2/ORDSTests.java
+++ src/test/java/com/automation/tests/day2/ORDSTests.java
@@ -1,13 +1,17 @@
 package com.automation.tests.day2;
-
+import io.restassured.RestAssured;
+import io.restassured.http.Header;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ORDSTests {
-    //address of ORDS web service, that is running no AWS ec2
-    //dara is coming frim SQL Oracle data base to this web service
+    //address of ORDS web service, that is running no AWS ec2.
+    //dara is coming from SQL Oracle data base to this web service
+    //during back-end testing with SQL developer and JDBC API
+    //we were accessing data base directly
+    //now, we gonna access web service
 
     private String baseURI = "http://ec2-52-90-22-4.compute-1.amazonaws.com:1000/ords/hr";
     //verify that status code is 200
@@ -20,5 +24,32 @@ public class ORDSTests {
         assertEquals(200,response.getStatusCode());
         System.out.println(response.prettyPrint());
 
+    }
+
+    @Test
+    public void test2(){
+        Response response = given().
+                header("accept","application/json").// doesn't it make differs?????
+                get(baseURI+"/employees/100");
+        int actualStatusCode = response.getStatusCode();
+        System.out.println(response.prettyPrint());
+        assertEquals(200,actualStatusCode);
+
+        System.out.println("what kind o contentent server sends to you, in this reponse: "+response.getHeader("Content-Type"));
+    }
+
+    //  #Task: perform Get request to /regions, print body and all headers.
+
+    @Test
+    public void test3 (){
+        Response response= given().get(baseURI+"/regions");
+        assertEquals(200,response.getStatusCode());
+        // to get specific header
+        Header header = response.getHeaders().get("content-Type");
+        for (Header h: response.getHeaders()){
+            System.out.println(h);
+        }
+        System.out.println("#########################################");
+        System.out.println(response.prettyPrint());
     }
 }
